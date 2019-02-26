a5c0dc0047d8a4de8d6a5d332687545edea2e628
unknown <lory_extr3me@hotmail.it>
2019-02-26 20:20:25 +0100

    first upload

---
+++ image_segmentation_main.py
@@ -0,0 +1,11 @@
+import os
+import cv2 as cv
+
+print("path: ", os.path.dirname(os.path.realpath(__file__)))
+print('image path:', end=' ')
+img_path = input()
+img = cv.imread(img_path, 0)
+
+img = cv.GaussianBlur(img, (11, 11), 2) # valore empirico, l'ideale sarebbe modificare i parametri in base alla "rumorosità dell'elemento in input
+thr,img = cv.threshold(img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU) # otsu thresholding
+cv.imwrite('test.tiff', img) # salva immagine nella cartella corrente
