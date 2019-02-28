032f38143f5fa08daa1ccd01d1036dc4a248ccf6
unknown <lory_extr3me@hotmail.it>
2019-02-28 22:48:22 +0100

    fixed readme + polygonize solutions

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ image_segmentation_main.py
 qgis_processing.py
 ```
 
-il primo (necessita l'installazione di `opencv-python`) segmenta, dato un path, un'immagine che viene successivamente convertita in formato __TIFF__.  
-il secondo script, lanciato all'interno dell'ambiente _QGIS_ (richiesta la versione 3 o superiore) genera lo shapefile (work in progress)
+il primo (necessita l'installazione di `opencv-python`) segmenta, dato un path, un'immagine che viene successivamente convertita in formato _TIFF_.  
+il secondo script, lanciato all'interno dell'ambiente _QGIS_ (richiesta la versione __3__ o superiore) genera lo shapefile (work in progress)
 
-![Alt diagram]("https://i.ibb.co/w04s7rv/diag.png")
+![Alt diagram](https://i.ibb.co/w04s7rv/diag.png)
 
-*si vogliono poi adattare gli script a ricevere in input una serie di immagini anziché una sola*
+__si vogliono poi adattare gli script a ricevere in input una serie di immagini anziché una sola__
--- qgis_processing.py
+++ qgis_processing.py
@@ -3,15 +3,15 @@
 import qgis.utils
 from qgis.core import *
 from pathlib import Path
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets # QGIS >= 3
 #from qgis.gui import QgsMapLayerActionRegistry
 from qgis.analysis import QgsRasterCalculator, QgsRasterCalculatorEntry
-#from osgeo import gdal, ogr, osr
+from osgeo import gdal, ogr#, osr
 
 # Load input dialog
 qid = QtWidgets.QInputDialog()
 title = "qgis_processing"
-label = "Enter path .TIFF:"
+label = "Enter TIFF path:"
 mode = QtWidgets.QLineEdit.Normal
 path, res = QtWidgets.QInputDialog.getText(qid, title, label, mode)
 
@@ -29,13 +29,13 @@ entries.append(band)
 
 # Raster calculation process
 calc = QgsRasterCalculator(band.ref, path, "GTiff", layer.extent(), layer.width(), layer.height(), entries)
-calc.processCalculation()
+calc.processCalculation() # creates a Raster GDAL-compatible (editable), overwrites old tiff file
 
 # Load Raster
 #qgis.utils.iface.addRasterLayer(path, "calc_raster")
 
-#######################################################################
-test = Path(path).parent/"test.shp"
+#########################no##############################################
+
 #sourceRaster = gdal.Open(test)
 #sr_proj=sourceRaster.GetProjection()
 #raster_proj = osr.SpatialReference()
@@ -51,6 +51,38 @@ test = Path(path).parent/"test.shp"
 #newField = ogr.FieldDefn(str(1), ogr.OFTInteger)
 #outLayer.CreateField(newField)
 
+#dst_layername = 'polygonized_test_all_all_2.shp'
+#drv = ogr.GetDriverByName("ESRI Shapefile")
+#dst_ds = drv.CreateDataSource(dst_layername)
+#dst_layer = dst_ds.CreateLayer(dst_layername, None)
+#fd = ogr.FieldDefn("DN", ogr.OFTInteger)
+#dst_layer.CreateField(fd)
+#dst_field = dst_layer.GetLayerDefn().GetFieldIndex("DN")
+#gdal.Polygonize(path, path, dst_layer, dst_field, [], callback=None)
+
+#################################################################################
+
+gdal.UseExceptions()
+src_ds = gdal.Open(path)
+if src_ds is None:
+    print('Unable to open %s' % src_filename)
+    sys.exit(1)
+
+try:
+    srcband = src_ds.GetRasterBand(1)
+except(RuntimeError, e):
+    print(e)
+    sys.exit(1)
+
+dst_layername = "POLYGONIZED"
+drv = ogr.GetDriverByName("ESRI Shapefile")
+dst_ds = drv.CreateDataSource(dst_layername + ".shp")
+dst_layer = dst_ds.CreateLayer(dst_layername, srs = None)
+
+# gdal.Polygonize(srcband, None, dst_layer, -1, [], callback=None)
+
 ############################################################################
 
 #gdal.Polygonize(path, path, Path(path).parent/"test.shp", 1)
+
+## shell command: python3 -m gdal_polygonize path\to\test.tiff path\to\OUTPUT.shp -b 1 -f "ESRI Shapefile" OUTPUT DN -mask path\to\test.tiff
